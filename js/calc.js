de7919f087bd72fec3057b64ad67eb66c77949a7
Matthew Shaw <mshaw@sparta.com>
2019-01-31 20:24:08 +0000

    number button working repeatedly

--- js/calc.js
+++ js/calc.js
@@ -5,10 +5,17 @@ for (var i = 0; i < buttonNum.length; i++) {
 
 var clearScreen = document.getElementsByClassName("buttonClear");
 clearScreen[0].addEventListener("click", buttonClear);
-
+var calculation = [];
 function numClick() {
-// alert("on");
+  var onScreen = document.getElementById("screen");
+  var data = this.innerHTML;
+
+  onScreen.innerHTML= onScreen.innerHTML + data;
+  console.log(onScreen.innerHTML);
+
 }
+
+
 function buttonClear(){
 // alert("on");
   var onScreen = document.getElementById("screen");
