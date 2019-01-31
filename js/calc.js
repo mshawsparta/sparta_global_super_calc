2d44c3fa467eb8e1f6eab2aac952c198a8ca9ecd
Matthew Shaw <mshaw@sparta.com>
2019-01-31 20:59:05 +0000

    working calculator

--- js/calc.js
+++ js/calc.js
@@ -5,19 +5,39 @@ for (var i = 0; i < buttonNum.length; i++) {
 
 var clearScreen = document.getElementsByClassName("buttonClear");
 clearScreen[0].addEventListener("click", buttonClear);
-var calculation = [];
+
+
+var symbol = document.getElementsByClassName("operator");
+for (var i = 0; i < symbol.length; i++) {
+  symbol[i].addEventListener("click", passOperator);
+}
+var onScreen = document.getElementById("screen");
+
+var equals = document.getElementsByClassName("equals");
+equals[0].addEventListener("click", doMaths);
+var num1 = 0;
 function numClick() {
-  var onScreen = document.getElementById("screen");
   var data = this.innerHTML;
 
   onScreen.innerHTML= onScreen.innerHTML + data;
   console.log(onScreen.innerHTML);
+  num1 = onScreen;
+  return num1;
 
 }
-
 
 function buttonClear(){
 // alert("on");
-  var onScreen = document.getElementById("screen");
   console.log(onScreen.innerHTML= "");
 }
+
+function passOperator(){
+  var data = this.innerHTML;
+  onScreen.innerHTML= onScreen.innerHTML + data;
+}
+
+function doMaths(){
+  onScreen.innerHTML = eval(num1.innerHTML);
+}
+
+// console.log(onScreen.innerHTML);
